e8a8ebb4ec38a38f8d728823e24ca43646300b93
mingyo <s2142717@gmail.com>
2023-05-26 16:29:39 +0800

    調整主頁連結路徑

--- js/switch.js
+++ js/switch.js
@@ -1,7 +1,20 @@
 const link = document.querySelector(".navbar-nav");
+const spot =document.querySelector(".spot");
+const food =document.querySelector(".food");
+const hotel =document.querySelector(".hotel");
 const loading = document.querySelector(".loading");
 
 link.addEventListener("click", e => {
+    sessionStorage.setItem("page", e.target.dataset.page);
+});
+
+spot.addEventListener("click", e => {
+    sessionStorage.setItem("page", e.target.dataset.page);
+});
+food.addEventListener("click", e => {
+    sessionStorage.setItem("page", e.target.dataset.page);
+});
+hotel.addEventListener("click", e => {
     sessionStorage.setItem("page", e.target.dataset.page);
 });
 
